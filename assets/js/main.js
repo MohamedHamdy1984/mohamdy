a46cb44ccc533ebb2ee4b22d6f51e1deaf75f901
Mohamed Hamdy <eng.mohamed.hamdy2010@gmail.com>
2024-02-26 11:08:24 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,44 @@
-// ********************   navbar
-// Change active nav-item according to position
-const sections = document.querySelectorAll('section');
-const links = document.querySelectorAll('header .nav-item a');
+// ********************   sidebar
+const sidebarAnchors = document.querySelectorAll('#sidebar ul.nav li a'),
+    sections = document.querySelectorAll('section'),
+    mainContent = document.querySelector('main');
 
-window.addEventListener('scroll', () => {
-    let scrollPosition = window.scrollY + 120;
-    // console.log(scrollPosition);
-    
 
+
+
+// Change active nav-item   on click event
+sidebarAnchors.forEach(sidebarAnchor => {
+    sidebarAnchor.lastElementChild.style.left = sidebarAnchor.offsetWidth - 2; // Add left property to CSS code of .extend div according to a width
+    sidebarAnchor.addEventListener('click', function(){
+        sidebarAnchors.forEach(sidebarAnchor => {
+            sidebarAnchor.classList.remove('active');
+        });
+        this.classList.add('active');
+        let sectionID = this.getAttribute('href');
+        let sectionOffest = document.querySelector(sectionID).offsetTop;
+        mainContent.scrollTo({
+            top: sectionOffest,
+            left: 0,
+            behavior: "smooth",
+          });
+    })
+})
+
+// Change active nav-item according to position on scroll event
+
+mainContent.addEventListener('scroll', () => {
+    let scrollPosition = mainContent.scrollTop + 120;
     sections.forEach(section => {
         if (scrollPosition >= section.offsetTop) {
-            links.forEach(link => {
-                link.classList.remove('active');
-                if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
-                    link.classList.add('active');
+            sidebarAnchors.forEach(sidebarAnchor => {
+                sidebarAnchor.classList.remove('active');
+                if (section.getAttribute('id') === sidebarAnchor.getAttribute('href').substring(1)) {
+                    sidebarAnchor.classList.add('active');
                 }
             });
         }
     });
-});
-sections.forEach(section => {
-    section.addEventListener('click', function(){
-        // links.forEach(link => {
-        //     link.classList.remove('active');
-        //     console.log(this);
-        // }) // end liks foreach
-        console.log('helloooooooooooooooooo')
-    }) // end section . addeventlistner
-}) // end sections.forEach
-for(let i=0; i<sections.length; i++){
-    sections[i].addEventListener('click', function(){
-        // links.forEach(link => {
-        //     link.classList.remove('active');
-        //     console.log(this);
-        // }) // end liks foreach
-        console.log('helloooooooooooooooooo')
-    }) // end section . addeventlistner
-}
+})
 
 // function onScroll(event){
 //     var scrollPos = $(document).scrollTop();
@@ -55,12 +57,6 @@ for(let i=0; i<sections.length; i++){
 // $(document).ready(function () {
 //     $(document).on("scroll", onScroll);
 // });
-
-
-
-
-
-
 
 
 
@@ -100,33 +96,6 @@ for (let i = 0; i < projects_title.length; i++) {
         }
     })
 }
-
-// let projects_title = document.querySelectorAll('#projects .projects-filter input');
-// let cards = document.querySelectorAll('#projects .card-box');
-// for (let i = 0; i < projects_title.length; i++) {
-//     projects_title[i].addEventListener("click", function(){
-//         // document.querySelector('#projects .projects-filter li.active')?.classList.remove('active')
-//         // this.classList.add('active');
-
-//         const value = this.getAttribute('data-filter');
-//         if(value == 'all'){
-//             cards.forEach(function(card){
-//                 card.classList.remove('hide');
-//                 card.classList.remove('order-last');
-//             })
-//         } else {
-//             cards.forEach(function(card){
-//                 if(card.classList.contains(value)){
-//                     card.classList.remove('hide');
-//                     card.classList.remove('order-last');
-//                 } else {
-//                     card.classList.add('hide');
-//                     card.classList.add('order-last');
-//                 }
-//             })
-//         }
-//     })
-// }
 
         // *********** same code but with  JQuery *********** //
 
